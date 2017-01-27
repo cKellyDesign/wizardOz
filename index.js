4b62a7c09dbb24d3706524bffedf382626f84644
Conor Kelly <ckelly@ckellydesign.net>
2017-01-27 11:52:20 -0800

    setting up basic socket connection

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ var connections = [];
 
 app.use(express.static('./public'));
 
+
 app.get('*', function (req, res) {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
 	console.log('Express Server running on port %s', this.address().port);
+});
+
+
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+	console.log('connection made!');
+
+	socket.once('disconnect', function() {
+		console.log('connection disconnected');
+		socket.disconnect();
+	})
+
+
 });
