637346784312c30303b1fc091bfc1d4e35a1d7a4
Conor Kelly <ckelly@ckellydesign.net>
2017-01-27 11:34:48 -0800

    init setup

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var _ = require('underscore');
+var app = express();
+
+var connections = [];
+
+app.use(express.static('./public'));
+
+app.get('*', function (req, res) {
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+	console.log('Express Server running on port %s', this.address().port);
+});
